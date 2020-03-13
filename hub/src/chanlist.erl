32ede9a81f5d2e35bc1970596c35185996a276a0
Hauke Stieler <mail@hauke-stieler.de>
2020-03-13 03:22:00 +0100

    Fix issue with byses/bychan lists

--- hub/src/chanlist.erl
+++ hub/src/chanlist.erl
@@ -10,42 +10,28 @@
 -include("hub.hrl").
 
 %%
-%% Helpers
-%%
-bySes() ->
-	"byses".
-
-byChan() ->
-	"bychan".
-
-new_chanlist() ->
-	%% TODO: somehow this need to be moved into chanlist.erl
-	ets:new(bySes(), [set, protected]),
-	ets:new(byChan(),  [bag, protected, {keypos, 2}]).
-
-%%
 %% online/offline
 %%
-set_online(ChanId, Pid) ->
-	ok = delete(chanid, ChanId),
-	insert(ChanId, Pid, online).
+set_online(List, ChanId, Pid) ->
+	ok = delete(chanid, ChanId, List),
+	insert(ChanId, Pid, online, List).
 
-set_offline(chanid, ChanId, Pid) ->
-	case lookup_raw(chanid, ChanId) of
+set_offline(chanid, List, ChanId, Pid) ->
+	case lookup_raw(chanid, ChanId, List) of
 		[Entry] ->
-			set_offline(entry, Entry, Pid);
+			set_offline(entry, Entry, Pid, List);
 		[] ->
-			insert(ChanId, Pid, offline)
+			insert(ChanId, Pid, offline, List)
 	end;
 
-set_offline(entry, Entry, Pid) ->
+set_offline(entry, Entry, Pid, List) ->
 	{ChanId, _, CurPid, CurState, _} = Entry,
 	case {CurPid, CurState} of
 		{_, offline} ->
 			ok;
 		{Pid, online} ->
-			ok = delete(chanid, ChanId),
-			insert(ChanId, Pid, offline);
+			ok = delete(chanid, ChanId, List),
+			insert(ChanId, Pid, offline, List);
 		{CurPid, online} ->
 			{new_pid, CurPid}
 	end.
@@ -53,25 +39,23 @@ set_offline(entry, Entry, Pid) ->
 %%
 %% Lookup and other info
 %%
-lookup(Type, Id) ->
-	{ok, fmt_lookup(lookup_raw(Type, Id))}.
 
-lookup() ->
-	{ok, fmt_lookup(lookup_raw())}.
+lookup(List) ->
+	ets:tab2list(list:nth(0, List)).
 
-lookup_raw(sesid, SesId) ->
-	ets:lookup(bySes(), SesId);
+lookup(Type, List, Id) ->
+	{ok, fmt_lookup(lookup_raw(Type, List, Id))}.
 
-lookup_raw(chanid, ChanId) ->
-	ets:lookup(byChan(), ChanId).
+lookup_raw(sesid, List, SesId) ->
+	ets:lookup(list:nth(1, List), SesId);
 
-lookup_raw() ->
-    ets:tab2list(byChan()).
+lookup_raw(chanid, List, ChanId) ->
+	ets:lookup(list:nth(0, List), ChanId).
 
-lookup_expired() ->
-	{ok, fmt_lookup(lookup_expired_raw())}.
+lookup_expired(List) ->
+	{ok, fmt_lookup(lookup_expired_raw(List))}.
 
-lookup_expired_raw() ->
+lookup_expired_raw(List) ->
 	Now = now_secs(),
 	OfflineExpire = Now - config:get(chan_expire),
 	OnlineExpire = OfflineExpire - config:get(poll_timeout),
@@ -84,29 +68,29 @@ lookup_expired_raw() ->
 						  _ ->
 							  Acc
 					  end
-			  end, [], byChan()).
+			  end, [], list:nth(0, List)).
 
-size() ->
-	Info = ets:info(byChan()),
+size(List) ->
+	Info = ets:info(list:nth(0, List)),
 	proplists:get_value(size, Info).
 
 %%
 %% List modifications
 %%
-insert(ChanId, Pid, State) ->
-	insert(ChanId, Pid, State, now_secs()).
+insert(ChanId, Pid, State, List) ->
+	insert(ChanId, Pid, State, now_secs(), List).
 
-insert(ChanId, Pid, State, Mtime) ->
+insert(ChanId, Pid, State, Mtime, List) ->
 	Entry = {ChanId, ChanId#hub_chan.sesid, Pid, State, Mtime},
-	ets:insert(byChan(), Entry),
-	ets:insert(bySes(), Entry),
+	ets:insert(list:nth(0, List), Entry),
+	ets:insert(list:nth(1, List), Entry),
 	ok.
 
-delete(Type, Id) ->
-	Entries = lookup_raw(Type, Id),
+delete(Type, Id, List) ->
+	Entries = lookup_raw(Type, Id, List),
 	lists:foreach( fun(Entry) ->
-						   ets:delete_object(byChan(), Entry),
-						   ets:delete_object(bySes(), Entry)
+						   ets:delete_object(list:nth(0, List), Entry),
+						   ets:delete_object(list:nth(1, List), Entry)
 				   end, Entries ),
 	ok.
 
